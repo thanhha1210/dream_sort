e03d79e9bf6d68432ce2f47430497d682c20bd2d
Nathan Huynh <nathanhuynh171005@gmail.com>
2025-03-15 14:01:54 -0700

    clean up

--- dream_sort_be/server.js
+++ dream_sort_be/server.js
@@ -14,7 +14,6 @@ const io = new Server(server, {
 });
 
 let sharedArray = [8, 5, 1, 2, 3]
-
 io.on("connection", (socket) => {
   console.log(socket.id)
   // Send the current array state to the newly connected client
@@ -26,10 +25,6 @@ io.on("connection", (socket) => {
   })
 
 })
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
