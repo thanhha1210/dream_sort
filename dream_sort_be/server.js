7ec6dd4161261cce3fd8b3f043a7bb0b8254fcca
Nathan Huynh <nathanhuynh171005@gmail.com>
2025-03-15 11:46:33 -0700

    setting up socket

--- dream_sort_be/server.js
+++ dream_sort_be/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const { createServer } = require('node:http');
+const { Server } = require("socket.io")
+const cors = require("cors");
 
 const app = express();
 const server = createServer(app);
 
+const io = new Server(server, {
+  cors:{
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
+
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
